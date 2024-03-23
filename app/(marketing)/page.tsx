1d2f66790286f1eec5aa126fece699d6465609a5
shixiong <shixiong327926@gmail.com>
2024-03-23 17:53:47 +0800

    learn 52:58

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,7 @@
 // 营销页面
 
 import { Medal } from "lucide-react"; //图标
+import { Button } from "@/components/ui/button"
 
 const MarketingPage = () => {
   return (
@@ -11,6 +12,19 @@ const MarketingPage = () => {
           <Medal className="h-6 w-6 mr-2" />
           No 1 task managment
         </div>
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+          Taskify helps team move
+        </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r 
+        from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4">
+          work forward
+        </div>
+      </div>
+      <div className="text-sm md:text-xl text-neutral-400 mt-4
+      max-w-xs md:max-w-2xl text-center mx-auto">
+      Collaborate, manage projects,and reach new productivity
+      peaks. From high rises to the home office, the way your team
+      works is unique - accomplish it all with Taskify.
       </div>
     </div>
   )
