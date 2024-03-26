06cd4c4909fb12181dd978f07c63c51ae829d82c
shixiong <shixiong327926@gmail.com>
2024-03-26 23:54:39 +0800

    learn 1:12:42

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,5 +1,7 @@
 // 	_folde选择文件夹和所有子段不参与路由
 import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const Navbar = () => {
   return (
@@ -7,6 +9,19 @@ export const Navbar = () => {
       <div className="md:max-w-screen-2xl mx-auto flex
       items-center w-full justify-between">
         <Logo />
+        <div className="space-x-4 md:block md:w-auto flex
+        items-center justify-between w-full">
+          <Button size="sm" variant="outline" asChild>
+            <Link href="/sign-in">
+              Login
+            </Link>
+          </Button>
+          <Button size="sm" asChild>
+            <Link href="/sign-up">
+              Get Taskify for free
+            </Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
