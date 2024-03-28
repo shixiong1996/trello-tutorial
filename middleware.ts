e1e76adf845865f6410333eaefa4383494953af2
shixiong <shixiong327926@gmail.com>
2024-03-29 00:43:33 +0800

    learn 1:31:16

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ['/']
 });
- 
+
 export const config = {
   matcher: [
     // Exclude files with a "." followed by an extension, which are typically static files.
