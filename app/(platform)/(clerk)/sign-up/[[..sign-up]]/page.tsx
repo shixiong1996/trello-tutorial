5c811d110f7377586676c05bde4840a572f67526
shixiong <shixiong327926@gmail.com>
2024-03-29 00:29:09 +0800

    learn 1:31:16

---
+++ app/(platform)/(clerk)/sign-up/[[..sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+// [[...folder]] 动态路线 可选的路线段
+
+import { SignUp } from "@clerk/nextjs";
+ 
+export default function Page() {
+  return <SignUp />;
+}
--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  // publicRoutes: ['/anyone-can-visit-this-route'],
+  publicRoutes: ['/']
 });
  
 export const config = {
