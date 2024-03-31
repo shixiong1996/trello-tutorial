0998569852d1626303a8648ac6955a38ea090f39
shixiong <shixiong327926@gmail.com>
2024-03-31 23:49:29 +0800

    learn 1:42:50

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,8 @@
+// <OrganizationList /> 组件用于向用户显示组织相关的成员资格、邀请和建议。
+import { OrganizationList } from "@clerk/nextjs";
+
+export default function CreateOrganizationPage() {
+  return (
+    <OrganizationList />
+  )
+}
--- app/(platform)/protected/page.tsx
+++ app/(platform)/protected/page.tsx
@@ -1,17 +1,14 @@
 // 客户端组件
 'use client'
 
-import { useAuth, useUser } from "@clerk/nextjs"
-
+import { UserButton } from "@clerk/nextjs"
 
 const ProtectedPage = () => {
-  const { userId } = useAuth()
-  const { user } = useUser()
-
   return (
     <div>
-      User: {user?.firstName}
-      userId: {userId}
+      <UserButton afterSignOutUrl="/">
+        
+      </UserButton>
     </div>
   )
 }
