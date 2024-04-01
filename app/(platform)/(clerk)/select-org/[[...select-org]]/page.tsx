a6d663ecbbbb32b1cf6a2a0180a14391fe76b527
shixiong <shixiong327926@gmail.com>
2024-04-01 23:58:56 +0800

    learn 1:46:42

--- app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -3,6 +3,10 @@ import { OrganizationList } from "@clerk/nextjs";
 
 export default function CreateOrganizationPage() {
   return (
-    <OrganizationList />
+    <OrganizationList 
+      hidePersonal
+      afterCreateOrganizationUrl='/organization/:id'
+      afterSelectOrganizationUrl='/organization/:id'
+    />
   )
 }
