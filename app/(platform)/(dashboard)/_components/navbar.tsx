93bd2ca34ac65be1bdc29f936c11f4195816eb51
shixiong <shixiong327926@gmail.com>
2024-04-04 02:10:00 +0800

    learn 1:52:57

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,8 +1,17 @@
+import { Logo } from "@/components/logo"
+import { Button } from "@/components/ui/button"
+
 export const Navbar = () => {
   return (
-    <div className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white items-center">
+    <div className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      {/* TODO：Mobile Sidebar */}
       <div className="flex items-center gap-x-4">
-        
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+        <Button size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
+          Create
+        </Button>
       </div>
     </div>
   )
