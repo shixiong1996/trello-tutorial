62e3a3dd6997046ccfe47e46ce9189bbb66ec586
shixiong <shixiong327926@gmail.com>
2024-04-06 23:52:58 +0800

    learn 11:54:46

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,6 +1,8 @@
 import { Logo } from "@/components/logo"
 import { Button } from "@/components/ui/button"
 
+import { Plus } from "lucide-react"
+
 export const Navbar = () => {
   return (
     <div className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
@@ -11,6 +13,9 @@ export const Navbar = () => {
         </div>
         <Button size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
           Create1
+        </Button>
+        <Button size="sm" className="rounded-sm block md:hidd">
+          <Plus/>
         </Button>
       </div>
     </div>
