c8a1f8e8c322e2ea7efb0a076bcc7ae2e9c6b18c
shixiong <shixiong327926@gmail.com>
2024-04-11 23:29:34 +0800

    learn 2:18:25

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,7 +1,23 @@
 // 客户端组件
 'use client'
 
-export const Sidebar  = () => {
+import Link from "next/link"
+import { Plus } from "lucide-react"
+import { useLocalStorage } from "usehooks-ts"
+import { useOrganization, useOrganizationList } from "@clerk/nextjs"
+
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import { Skeleton } from "@/components/ui/skeleton"
+import { Accordion } from "@/components/ui/accordion"
+
+interface SidebarProps {
+  storageKey?: string
+}
+
+export const Sidebar  = ({
+  storageKey,
+}: SidebarProps ) => {
   return (
     <div>
       sidebar!
