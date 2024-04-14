32566dc9e3ca42a2ec27a332bb6a08432ea2cb9f
shixiong <shixiong327926@gmail.com>
2024-04-14 23:50:30 +0800

    learn 2:20:25

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -15,12 +15,17 @@ interface SidebarProps {
   storageKey?: string
 }
 
-export const Sidebar  = ({
-  storageKey,
-}: SidebarProps ) => {
+export const Sidebar = ({
+  storageKey = "t-sidebar-state"
+}: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  )
+
   return (
     <div>
-      sidebar!
+      Sidebar!
     </div>
   )
 }
