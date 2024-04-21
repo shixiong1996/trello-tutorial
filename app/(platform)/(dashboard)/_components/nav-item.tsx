44dd6640f2c1bae5cbfae3b597583653a4c576f7
shixiong <shixiong327926@gmail.com>
2024-04-21 23:56:04 +0800

    learn 11:54:46

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,5 +1,19 @@
 'use client'
 
+type organization = {
+  id: string
+  sulg: string
+  imageUrl: string
+  name: string
+}
+
+interface NavItemProps {
+  isExpanded: boolean
+  isActive: boolean
+  organizationId: organization
+  onExpand: (id: string) => void
+}
+
 export const NavItem = () => {
   return (
     <div>NavItem</div>
