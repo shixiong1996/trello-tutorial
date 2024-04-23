d1b0f1478d3e97ce99cf9aae8aaae67bb237320d
shixiong <shixiong327926@gmail.com>
2024-04-24 00:11:30 +0800

    learn 2:42:41

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,36 +1,39 @@
-'use client'
+"use client";
 
-import { useRouter, usePathname} from "next/navigation";
-import Image from "next/image"
+import { useRouter, usePathname } from "next/navigation";
+import Image from "next/image";
 import { Activity, CreditCard, Layout, Settings } from "lucide-react";
-import { AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 import { cn } from "@/lib/utils";
-import { use } from "react";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export type Organization = {
-  id: string
-  slug: string
-  imageUrl: string
-  name: string
-}
+  id: string;
+  slug: string;
+  imageUrl: string;
+  name: string;
+};
 
 interface NavItemProps {
-  isExpanded: boolean
-  isActive: boolean
-  organization: Organization
-  onExpand: (id: string) => void
+  isExpanded: boolean;
+  isActive: boolean;
+  organization: any;
+  onExpand: (id: string) => void;
 }
 
 export const NavItem = ({
   isExpanded,
   isActive,
   organization,
-  onExpand
-}:
-  NavItemProps
-) => {
-  const router = useRouter()
-  const pathname = usePathname()
+  onExpand,
+}: NavItemProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
 
   const routes = [
     {
@@ -55,6 +58,10 @@ export const NavItem = ({
     },
   ];
 
+  const onClick = (href: string) => {
+    router.push(href);
+  };
+
   return (
     <AccordionItem value={organization.id} className="border-none">
       <AccordionTrigger
@@ -72,10 +79,38 @@ export const NavItem = ({
               alt="Organization"
               className="rounded-sm object-cover"
             />
-            <span className="font-medium text-sm">{organization.name}</span>
           </div>
+          <span className="font-medium text-sm">{organization.name}</span>
         </div>
       </AccordionTrigger>
+      <AccordionContent className="pt-1 text-neutral-700">
+        {routes.map((route) => (
+          <Button
+            key={route.href}
+            size="sm"
+            onClick={() => onClick(route.href)}
+            className={cn(
+              "w-full font-normal justify-start pl-10 mb-1",
+              pathname === route.href && "bg-sky-500/10 text-sky-700"
+            )}
+            variant="ghost"
+          >
+            {route.icon}
+            {route.label}
+          </Button>
+        ))}
+      </AccordionContent>
     </AccordionItem>
-  )
-}
+  );
+};
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
+  );
+};
