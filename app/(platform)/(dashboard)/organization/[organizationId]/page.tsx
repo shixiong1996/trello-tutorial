67194e374acdf4cef4362247eaa946dfb9e28adf
shixiong <shixiong327926@gmail.com>
2024-05-14 23:45:34 +0800

    learn 3:16:58

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,19 @@
+'use client';
+
 const OrganizationIdPage = () => {
+  
+
   return (
     <div>
-      OrganizationId Page
+      <form action="">
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+      </form>
     </div>
   );
 };
