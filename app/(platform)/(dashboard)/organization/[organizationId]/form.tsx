375f33fa80bed888f93395aeaf8b54c10310acb3
shixiong <shixiong327926@gmail.com>
2024-05-22 17:46:54 +0800

    learn

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -8,6 +8,8 @@ export const Form = () => {
   const initialState = { message: "", errors: {} }
   // state当前的状态 dispatch新动作 create提交表单后设置为提供操作的返回值 initialState初始状态
   // 提交表单后，将调用您提供的create函数。它的返回值将成为表单的新的state。
+  // 当用户填写表单并提交时，dispatch 函数会被调用，执行 create 函数。
+  // 根据 create 函数的执行结果（比如成功、失败或错误信息的返回），state 会相应更新，以反映当前表单的状态，这包括显示错误或提交成功的消息。
   const [state, dispatch] = useFormState(create, initialState)
 
   return (
