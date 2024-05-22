d5b0483ee9f86ebd01ec39c6f05045d3fb3c75d9
shixiong <shixiong327926@gmail.com>
2024-05-22 10:36:30 +0800

    Update form.tsx

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -2,13 +2,13 @@
 
 import { create } from "@/action/create-board"
 import { Button } from "@/components/ui/button"
-import { useActionState } from "react";
+import { useFormState } from "react-dom";
 
 export const Form = () => {
   const initialState = { message: "", errors: {} }
   // state当前的状态 dispatch新动作 create提交表单后设置为提供操作的返回值 initialState初始状态
   // 提交表单后，将调用您提供的create函数。它的返回值将成为表单的新的state。
-  const [state, dispatch] = useActionState(create, initialState)
+  const [state, dispatch] = useFormState(create, initialState)
 
   return (
     <form action={dispatch}>
