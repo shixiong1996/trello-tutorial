6014393fdbd0ae126d3cf11503f37d0ac9684a26
shixiong <shixiong327926@gmail.com>
2024-05-25 17:34:10 +0800

    learn 4:01

---
+++ action/create-board/schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const CreateBoard = z.object({
+  title: z.string({
+    required_error: "需要输入标题",
+    invalid_type_error: "需要输入标题"
+  }).min(3, {
+    message: "标题至少需要3个字符"
+  })
+})
--- lib/caeate-safe-action.ts
+++ lib/caeate-safe-action.ts
@@ -1,3 +1,6 @@
+// 安全操作处理器
+import { z } from "zod";
+
 export type FieldErrors<T> = {
   [key in keyof T]?: string[];
 };
@@ -8,3 +11,16 @@ export type ActionState<TInput, TOutput> = {
   data?: TOutput;
 }
 
+export const createSafeAction = <TInput, TOutput>(
+  schema: z.Schema<TInput>,
+  handler: (validateData: TInput) => Promise<ActionState<TInput, TOutput>>
+) => {
+  return async (data: TInput): Promise<ActionState<TInput, TOutput>> => {
+    const validateData = schema.safeParse(data);
+    if(!validateData.success) {
+      return { fieldError: validateData.error.flatten().fieldErrors as FieldErrors<TInput> };
+    }
+
+    return handler(validateData.data);
+  }
+}
