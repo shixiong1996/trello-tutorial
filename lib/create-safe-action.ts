34f8d98987c360ec55fe142a606e8c64438ab467
shixiong <shixiong327926@gmail.com>
2024-06-05 00:01:39 +0800

    Update create-safe-action.ts

--- lib/create-safe-action.ts
+++ lib/create-safe-action.ts
@@ -4,18 +4,38 @@
 // zod 是一个用于验证和解析数据的库
 import { z } from 'zod'
 
-// 
+// 定义了一个泛型，可以将其他对象的所有属性值都改成 string数组 类型。
 export type FieldErrors<T> = {
-	[K in keyof T]?: string[]
+	[K in keyof T]?: string[] // 类型映射
 }
+
+// 假设以下数据类型符合fireldErrors的类型
+// FormFields : {
+// 	username: string
+// 	password: string
+// }
+// const formFiledErrors: FieldErrors<FormFields> = {
+// 	username: ['用户名不能为空'],
+// 	password: ['密码不能为空']
+// }
 
 export type ActionState<TInput, TOutput> = {
-	fieldErrors?: FieldErrors<TInput>,
-	error?: string | null,
-	data?: TOutput
+	fieldErrors?: FieldErrors<TInput>, // 存储与 TInput 类型字段相关的错误信息
+	error?: string | null,  // 存储错误信息
+	data?: TOutput // 存储处理后的数据
 }
 
-export const createSafeAction = <TInput, TOutput>(schema: z.Schema<TInput>, 
+// 假设以下数据类型符合ActionState的类型
+// const actionState: ActionState<FormFields, user> = { 
+// 	fieldErrors: {
+// 		username: ['用户名不能为空'],
+// 		password: ['密码不能为空']
+// 	},
+// 	error: '用户名或密码错误',
+// 	data: { id: 1, username: 'admin', email: 'admin@com' }
+// }
+
+export const createSafeAction = <TInput, TOutput>(schema: z.Schema<TInput>,
 	handler: (validatedDate: TInput) => Promise<ActionState<TInput, TOutput>>) => {
 	return async (data: TInput): Promise<ActionState<TInput, TOutput>> => {
 		const validationResult = schema.safeParse(data)
