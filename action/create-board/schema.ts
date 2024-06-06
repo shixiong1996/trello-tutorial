51683afc4756e4fab9fb3f08060ed61038997cc9
shixiong <shixiong327926@gmail.com>
2024-06-06 23:59:51 +0800

    Update schema.ts

--- action/create-board/schema.ts
+++ action/create-board/schema.ts
@@ -1,11 +1,14 @@
-// 验证
+// 数据验证模式
 
 import { z } from "zod";
 
+// 创建一个对象模式
 export const CreateBoard = z.object({
   title: z.string({
-    required_error: "需要输入标题",
-    invalid_type_error: "需要输入标题"
+    // 此消息会在 title 字段为空或未提供时被抛出。
+    required_error: "title不能为空",
+    // 此消息会在 title 字段存在但不是字符串时被抛出。
+    invalid_type_error: "title必须是字符串类型"
   }).min(3, {
     message: "标题至少需要3个字符"
   })
