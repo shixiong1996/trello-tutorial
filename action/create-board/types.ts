7965bc49a13dc32b6fb2d406487cb8c03a3357e5
shixiong <shixiong327926@gmail.com>
2024-06-07 16:04:35 +0800

    Update types.ts

--- action/create-board/types.ts
+++ action/create-board/types.ts
@@ -1,10 +1,9 @@
 // 保存类型
 import { z } from "zod";
-import { board } from "@prisma/client";
+import { board } from "@prisma/client"; // 引入prisma生成的类型
 
 import { ActionState } from "@/lib/create-safe-action";
-
 import { CreateBoard } from "./schema";
 
-export type InputType = z.infer<typeof CreateBoard>
-export type ReturnType = ActionState<InputType, board>
+export type InputType = z.infer<typeof CreateBoard> // 提取出推断的类型
+export type ReturnType = ActionState<InputType, board> // <提取出推断的类型, 数据库模型>
