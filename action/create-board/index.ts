a634063f737e468ba604baaf842ea9459c14f41d
shixiong <shixiong327926@gmail.com>
2024-06-11 00:13:36 +0800

    Update index.ts

--- action/create-board/index.ts
+++ action/create-board/index.ts
@@ -11,29 +11,30 @@ import { revalidatePath } from 'next/cache' // 重新验证某个路径的数据
 import { CreateBoard } from "./schema"; // 引入数据验证模式
 
 const handler = async ( data: InputType ): Promise<ReturnType> => {
-  const { userId } = auth()
+  const { userId } = auth() // 获取当前用户id
 
-  if(!userId) {
+  if(!userId) { // 如果id不存在 表示未授权
     return { error: '未授权' }
   }
 
+  // 从输入数据中提取出title
   const { title } = data
 
-  let board;
+  let board; // 用于存储创建的看板
 
   try {
     // throw new Error('测试')
-    board = await db.board.create({
+    board = await db.board.create({ // 创建看板 如果成功，将结果赋值给board。
       data: {
         title,
       }
     })
-  } catch (error) {
+  } catch (error) { // 如果出现错误，返回一个错误消息。
     return { error: '创建失败' }
   }
 
   revalidatePath('/board/${board.id}')
-  return { data: board }
+  return { data: board } // 新创建的看板数据
 }
 
 export const createBoard = createSafeAction(CreateBoard, handler)
