366dcfc67f24cdda3ee93fc4bc1ba6a8fccba11f
shixiong <shixiong327926@gmail.com>
2024-06-13 00:02:11 +0800

    form components

--- components/form/form-input.tsx
+++ components/form/form-input.tsx
@@ -1,10 +1,14 @@
 'use client'
 
 import { forwardRef } from "react"
+import { useFormStatus } from "react-dom"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { cn } from "@/lib/utils"
 
 interface FormInputProps {
   id: string,
-  lable?: string,
+  label?: string,
   type?: string,
   placeholder?: string,
   required?: boolean,
@@ -12,20 +16,52 @@ interface FormInputProps {
   errors?: Record<string, string[] | undefined>,
   className?: string,
   defaultValue?: string,
-  onBlure?: () => void,
+  onBlur?: () => void,
 }
 
-export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(({
-  id,
-  label,
-  type,
-  placeholder,
-  required,
-  disabled,
-  errors,
-  className,
-  defaultValue,
-  onBlur,
-}, ref)) => {
-  const 
-}
+export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
+  ({
+    id,
+    label,
+    type,
+    placeholder,
+    required,
+    disabled,
+    errors,
+    className,
+    defaultValue = '',
+    onBlur
+  }, ref) => {
+
+    const { pending } = useFormStatus()
+    return (
+      <div>
+        <div>
+          {label ?
+            <Label
+              htmlFor={id}
+              className="text-xs font-semibold text-neutral-700"
+            >
+              {label}
+            </Label>
+            : null}
+          <Input
+            onBlur={onBlur}
+            defaultValue={defaultValue}
+            ref={ref}
+            required={required}
+            name={id}
+            id={id}
+            placeholder={placeholder}
+            type={type}
+            disabled={pending || disabled}
+            className={cn("text-sm px-2 py-1 h-7", className)}
+            aria-describedby={`${id}-error`}
+          />
+        </div>
+        <FormErrors id={id} error={errors}></FormErrors>
+      </div>
+    )
+  })
+
+FormInput.displayName = "FormInput"
