6a978c419b80ea16ad6d5f13d328d31392c3064a
shixiong <shixiong327926@gmail.com>
2024-06-12 15:53:24 +0800

    Update form.tsx

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -12,13 +12,14 @@ import { useAction } from "@/hook/use-action"; // 处理异步操作，并管理
 export const Form = () => {
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
-      console.log(data, 'success')
+      console.log(data, 'success') // 回调在操作成功时被调用，并接收成功的数据。
     },
-    onError: (error) => {
+    onError: (error) => { // 回调在操作失败时被调用，并接收错误信息。
       console.error(error)
     }
   })
 
+  // 表单提交处理函数
   const onSubmit = (formData: FormData) => {
     const title = formData.get('title') as string
     execute({ title })
