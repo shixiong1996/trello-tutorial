d4919ccadf713485129172ed2cccab2d1414c82b
shixiong <shixiong327926@gmail.com>
2024-06-19 11:18:46 +0800

    learn

--- components/form/form-submit.tsx
+++ components/form/form-submit.tsx
@@ -1,3 +1,5 @@
+// 提交按钮
+
 'use client'
 
 import { useFormStatus } from "react-dom"
@@ -6,7 +8,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 
 interface FormSubmitProps {
-  children: React.ReactNode;
+  children: React.ReactNode; // React节点，通常用于定义组件内部显示的内容。
   disabled?: boolean;
   className?: string;
   variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | "primary";
@@ -18,7 +20,7 @@ export const FormSubmit = ({
   className,
   variant
 }: FormSubmitProps) => {
-  const { pending } = useFormStatus()
+  const { pending } = useFormStatus() // 获取表单的状态，pending是一个布尔值，表示表单是否正在提交中。
 
   return (
     <Button
