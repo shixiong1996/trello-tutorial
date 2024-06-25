338c029e8cfea76c719dad218c1bc392b1090025
shixiong <shixiong327926@gmail.com>
2024-06-25 23:34:32 +0800

    Update use-action.ts

--- hook/use-action.ts
+++ hook/use-action.ts
@@ -55,7 +55,6 @@ export const useAction = <TInput, TOutput>(
         options.onComplete?.()
       }
     },
-    
     [action, options] // useCallback 的依赖数组，确保当 action 或 options 改变时，重新生成 execute 函数
   )
   return {
