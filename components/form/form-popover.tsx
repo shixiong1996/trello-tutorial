a68104a5a64db005f8fb980f9d4ce87d07a64a11
shixiong <shixiong327926@gmail.com>
2024-07-17 23:54:09 +0800

    learn

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -14,6 +14,7 @@ import { createBoard } from "@/action/create-board"
 
 import { FormInput } from "./form-input"
 import { FormSubmit } from "./form-submit"
+import { FormPicker } from "./form-picker"
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -69,6 +70,10 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={onSubmit} className="space-y-4">
           <div className="space-y-4">
+            <FormPicker
+              id="image"
+              errors={fieldErrors}
+            />
             <FormInput
               id="title"
               label="Board title"
