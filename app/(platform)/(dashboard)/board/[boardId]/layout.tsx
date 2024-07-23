daad6eb59926e1b5477b1b111bb5c6ff2ff25eef
shixiong <shixiong327926@gmail.com>
2024-07-23 23:25:02 +0800

    learn

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,68 @@
+import { db } from "@/lib/db"
+import { notFound, redirect } from "next/navigation"
+import { auth } from "@clerk/nextjs"
+import { title } from "process"
+
+export async function generateMetadata({
+  params
+} :{
+  params: { boardId: string }
+}) {
+  const { orgId } = auth()
+
+  if(!orgId) {
+    return {
+      title: "Board"
+    }
+  }
+
+  const board = await db.board.findFirst({
+    where: {
+      id: params.boardId,
+      orgId
+    }
+  })
+
+  return {
+    title: board?.title || "Board"
+  }
+
+}
+
+const BoardIdLayout = async ({
+  children,
+  params
+}: {
+  children: React.ReactNode
+  params: { boardId: string }
+}) => {
+  const { orgId } = auth()
+
+  if (!orgId) {
+    redirect("/select-org")
+  }
+
+  const board = await db.board.findFirst({
+    where: {
+      id: params.boardId,
+      orgId
+    }
+  })
+
+  if (!board) {
+    notFound() // 404
+  }
+
+  return (
+    <div
+      className="relative h-full bg-no-repeat bg-cover bg-current"
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+    >
+      <main className="relative pt-28 h-full">
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default BoardIdLayout
--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { startCase } from "lodash" // 转换string为开头字母大写
+import { auth } from "@clerk/nextjs"
+
 import OrgContol from "./_components/org-contol"
+
+export async function generateMetadata() {
+  const { orgSlug } = auth()
+
+  return {
+    title: startCase(orgSlug || 'Organization'),
+  }
+}
 
 const OrganizationIdLayout = ({
   children
