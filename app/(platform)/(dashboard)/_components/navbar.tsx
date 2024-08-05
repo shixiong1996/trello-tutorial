9a5ef6e54203706ad7a993e957c89e0eeb910ee4
shixiong <shixiong327926@gmail.com>
2024-08-06 00:20:09 +0800

    learn

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -5,21 +5,26 @@ import { Button } from "@/components/ui/button"
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs" // 组织切换器
 
 import { MobileSidebar } from "./mobile-sidebar" // 移动侧边栏
+import { FormPopover } from "@/components/form/form-popover"
 
 const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      <MobileSidebar/>
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
         </div>
-        <Button variant="primary" size="sm" className="rounded-sm hidden md:block h-auto py-2 px-3">
-          Create
-        </Button>
-        <Button variant="primary" size="sm" className="rounded-sm block md:hidd">
-          <Plus className="h-5 w-5" />
-        </Button>
+        <FormPopover>
+          <Button variant="primary" size="sm" className="rounded-sm hidden md:block h-auto py-2 px-3">
+            Create
+          </Button>
+        </FormPopover>
+        <FormPopover>
+          <Button variant="primary" size="sm" className="rounded-sm block md:hidd">
+            <Plus className="h-5 w-5" />
+          </Button>
+        </FormPopover>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
         <OrganizationSwitcher
