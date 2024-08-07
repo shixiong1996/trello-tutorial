fa51bdc3ed37c86dd9d8d1f361328934e5e546f5
shixiong <shixiong327926@gmail.com>
2024-08-07 17:56:19 +0800

    learn

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -1,17 +1,23 @@
 "use client"
 
+import { toast } from "sonner"
+
 import { Plus, X } from "lucide-react"
 import { useState, useRef, ElementRef } from "react"
-import { useParams } from "next/navigation"
+import { useParams, useRouter } from "next/navigation"
 import { useEventListener, useOnClickOutside } from "usehooks-ts"
 
+import { useAction } from "@/hook/use-action"
+import { createList } from "@/action/create-list"
+
+import { Button } from "@/components/ui/button"
 import { FormInput } from "@/components/form/form-input"
 import { FormSubmit } from "@/components/form/form-submit"
-import { Button } from "@/components/ui/button"
 
 import { ListWrapper } from "./list-wrapper"
 
 export const ListForm = () => {
+  const router = useRouter()
   const params = useParams()
 
   const inputRef = useRef<ElementRef<"input">>(null)
@@ -30,6 +36,17 @@ export const ListForm = () => {
     setIsEditing(false)
   }
 
+  const { execute, fieldErrors } = useAction(createList, {
+    onSuccess: (data) => {
+      toast.success(`List ${data.title} created!`)
+      disableEditing()
+      router.refresh()
+    },
+    onError: (error) => {
+      toast.error(error)
+    }
+  })
+
   const onKeyDown = (e: KeyboardEvent) => { // 用于监听键盘按键
     if(e.key === "Escape") {
       disableEditing()
@@ -41,15 +58,24 @@ export const ListForm = () => {
   // 当用户点击页面其他地方时，调用 disableEditing 函数
   useOnClickOutside(formRef, disableEditing)
 
+  const onSumbit = (formData: FormData) => {
+    const title = formData.get("title") as string
+    const boardId = formData.get("boardId") as string
+
+    execute({ title, boardId })
+  }
+
   if(isEditing) {
     return (
       <ListWrapper>
         <form
+          action={onSumbit}
           ref={formRef}
           className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
         >
           <FormInput
             ref={inputRef}
+            errors={fieldErrors}
             id="title"
             className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
             placeholder="输入列表标题"
