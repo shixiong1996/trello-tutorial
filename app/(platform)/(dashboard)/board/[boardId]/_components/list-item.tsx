d0ddc72f979a408879c7ea80064c010e99a8945b
shixiong <shixiong327926@gmail.com>
2024-08-08 11:48:34 +0800

    learn

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { ListWithCards } from "@/types";
+
+interface ListItemProps {
+  data: ListWithCards;
+  index: number
+}
+
+export const ListItem = () => {
+  return (
+    <div>
+      list item
+    </div>
+  )
+}
