e15f7936afbf40012aa6076ba3a773c6e22de701
shixiong <shixiong327926@gmail.com>
2024-08-15 11:50:12 +0800

    learn

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import { forwardRef } from "react"; // forwardRef 允许组件使用 ref 将 DOM 节点暴露给父组件。
+import { Plus, X } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { FormTextarea } from "@/components/form/form-textarea";
+import { FormSubmit } from "@/components/form/form-submit";
 
 interface CardFormProps {
   listId: string;
@@ -18,6 +20,33 @@ export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(({
   disableEditing,
   isEditing
 }, ref) => {
+  if(isEditing) {
+    return (
+      <form className="m-1 py-0.5 px-1 space-y-4">
+        <FormTextarea
+          id="title"
+          onKeyDown={() => {}}
+          ref={ref}
+          placeholder="为这张卡片添加标题"
+        />
+        <input
+          hidden
+          id="listId"
+          name="listId"
+          value={listId}
+        />
+        <div className="flex items-center gap-x-1">
+          <FormSubmit>
+            Add card
+          </FormSubmit>
+          <Button onClick={disableEditing} size="sm" variant="ghost">
+            <X className="h-5 w-5" />
+          </Button>
+        </div>
+      </form>
+    )
+  }
+
   return (
     <div>
       <Button
