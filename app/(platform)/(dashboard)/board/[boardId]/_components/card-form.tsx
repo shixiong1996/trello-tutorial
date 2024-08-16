b681161984bfd9d30c55faeddc7f676d765dcdef
shixiong <shixiong327926@gmail.com>
2024-08-16 09:20:02 +0800

    learn

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -58,7 +58,7 @@ export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(({
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string
     const listId = formData.get("listId") as string
-    const boardId = formData.get("boardId") as string
+    const boardId = params.boardId as string
 
     execute({ title, listId, boardId })
   }
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -2,9 +2,12 @@
 
 import { useState, useRef, ElementRef } from "react";
 
+import { cn } from "@/lib/utils";
 import { ListWithCards } from "@/types";
-import { ListHeader } from "./list-header";
+
 import { CardForm } from "./card-form";
+import { CardItem } from "./card-item";
+import { ListHeader } from "./list-header";
 
 interface ListItemProps {
   data: ListWithCards;
@@ -37,6 +40,21 @@ export const ListItem = ({
         onAddCard={enableEditing}  
         data={data}
       />
+      <ol
+        className={cn(
+          "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+          data.cards.length > 0 ? "mt-2" : "mt-0"
+        )}
+      >
+        {data.cards.map((card, index) => (
+          <CardItem
+            index={index}
+            key={card.id}
+            data={card}
+          />
+        ))}
+
+      </ol>
       <CardForm
         ref={textareaRef}
         listId={data.id}
