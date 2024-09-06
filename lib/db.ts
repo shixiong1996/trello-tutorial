64c50f7d70cdf27bc113a981ce2647623cc42003
shixiong <shixiong327926@gmail.com>
2024-09-06 11:31:50 +0800

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -11,7 +11,7 @@ declare global {
 // 实例化PrismaClient 检查prisma是否已经实例化，如果没有实例化，我们就实例化一个，以免重复创建实例
 // globalThis是全局对象，我们可以在任何地方访问它
 export const db = globalThis.prisma || new PrismaClient({
-  errorFormat: 'pretty'
+  errorFormat: 'pretty' // 启用美观的错误格式。这种格式通常会提供更友好的输出，便于阅读和理解错误信息，适合开发环境中的调试。
 })
 
 // 如果是生产环境，我们将全局变量globalThis.prisma设置为db 任何地方都可以访问数据库
